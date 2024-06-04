cbc86abce8a68273718bdb8db97c17e9395118c3
Kulsoom Saboor <kulsooms@DESKTOP-KD5LPRQ>
2024-06-04 20:53:43 +0500

    created fast-api endpoint for subnet-01

---
+++ app/main.py
@@ -0,0 +1,39 @@
+
+from protocol import StreamPromptingSynapse
+
+from pydantic import BaseModel
+from typing import List
+
+from fastapi import FastAPI
+import uvicorn
+
+from prompt import query_network
+
+
+app = FastAPI()
+
+class InputData(BaseModel):
+    roles: List[str]=["system", "user"]
+    messages: List[str]=["Write an essay of 100 words on World War 1", "Write an essay of 100 words on World War 1"]
+
+
+@app.post("/text-to-text", responses={200:{"description":"text to speech"}})
+async def text_to_text(input_data: InputData):
+
+    messages=input_data.messages
+    roles=input_data.roles
+
+    synapse = StreamPromptingSynapse(
+    roles=roles,
+    messages=messages,
+    completion="",  # will be filled after processing
+    required_hash_fields=["messages"],
+    timeout=500.0,  # timeout in seconds
+    )
+
+    return await query_network(synapse)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8001)
+
---
+++ app/prompt.py
@@ -0,0 +1,54 @@
+
+import os
+from dotenv import load_dotenv
+
+from bittensor import Keypair, metagraph, wallet,  logging
+import bittensor as bt
+
+
+import asyncio, traceback
+
+
+async def query_network(synapse):
+         # Configuration and Setup
+
+        load_dotenv()  # This loads the environment variables from the .env file
+        mnemonic = os.getenv('MNEMONIC')
+        wallet = Keypair.create_from_mnemonic(mnemonic)
+        bt.trace()
+
+        metagraph = bt.metagraph(netuid=1, sync=True, lite=False)
+        logging.info( f"METAGRAPH SYNCED", )
+
+        den = bt.dendrite(wallet)
+        logging.debug(f"dendrite: {den}")
+
+        best_miner = max(range(metagraph.n), key=lambda uid: metagraph.I[uid].item())
+        print(best_miner)
+
+        axon_to_query = metagraph.axons[best_miner]
+        logging.debug(f"Axons to query: {axon_to_query}")
+
+
+        response = await den(
+            axon_to_query,
+            synapse=synapse,
+            deserialize=False,
+            timeout=500.0,
+            streaming=True,
+        )
+
+
+        synapses = []
+        synapse=""
+        try:
+            ii = 0
+            async for chunk in response:
+                print(f"\nchunk {ii}: {chunk}", end="", flush=True)
+                ii += 1
+                synapse = chunk  # last object yielded is the synapse itself with completion filled
+
+            synapses.append(synapse)
+        except Exception as e:
+            print(f"\nError processing response: {traceback.format_exc()}")
+        return synapses
